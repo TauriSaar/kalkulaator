b0030b840b96e2cbb7f6bdc1f8118547654927a6
Tauri <tauri.saar@voco.ee>
2023-05-03 22:23:43 +0300

    Harjutus 2

---
+++ __tests__/komplekt6.ts
@@ -0,0 +1,25 @@
+import {Calculator} from "../calculator1";
+let calcobj:Calculator=null;
+
+beforeEach(() => {
+    calcobj=new Calculator();
+});
+
+test('simple input', ()=>{
+    calcobj.pressButton('7');
+    expect(calcobj.getPanelContents()).toBe("7");
+});
+
+test('simple input', ()=>{
+    calcobj.pressButton('8');
+    expect(calcobj.getPanelContents()).toBe("8");
+});
+
+test('simple input', ()=>{
+    calcobj.pressButton('9');
+    expect(calcobj.getPanelContents()).toBe("9");
+});
+
+test('empty init', () => {
+    expect(calcobj.getPanelContents()).toBe("");
+});
